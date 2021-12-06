6e83a91f52f515f58be9a18d07044bd058331ee0
farah <farah.fournat@gmail.com>
2021-12-06 22:04:00 +0100

    birthday reminder setup

---
+++ setup/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return <main>
+    <section className="container">
+      <h3>0 birthdays today</h3>
+      <List />
+      <button onClick={()=> console.log('hello')}>clear all</button>
+    </section>
+  </main>;
+}
+
+export default App;
